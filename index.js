342004d1d08dfbaf912b15eb5300f60f33e5f625
j-shilling <shilling.jake@gmail.com>
2018-12-19 08:52:34 -0500

    solution

--- index.js
+++ index.js
@@ -1,7 +1,24 @@
 function findMinAndRemove(array){
+    if (array.length === 0)
+	return null;
+    
+    let min_index = 0;
+    for (let i = 0; i < array.length; i++) {
+	if (array[i] < array[min_index]) {
+	    min_index = i;
+	}
+    }
 
+    return array.splice(min_index, 1)[0];
 }
 
 function insertionSort(array){
+    let copy = array.slice();
+    let ret = [];
+    
+    while (copy.length > 0) {
+	ret.push(findMinAndRemove(copy));
+    }
 
+    return ret;
 }
